52c3385fbb8769b3748c64ef845f92eb49dfe351
Lorenzo Ricelli <lorenzo.ricelli@gmail.com>
2016-09-16 15:17:06 +0200

    Aggiunti parametri titolare e cliente

--- app/controllers/clienti_controller.rb
+++ app/controllers/clienti_controller.rb
@@ -69,6 +69,7 @@ class ClientiController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def cliente_params
-      params.fetch(:cliente, {})
+      #params.fetch(:cliente, {})
+      params.require(:cliente).permit(nome,:cognome,:cf,:data_nascita,:telefono,:email,:actable_id,:actable_type))
     end
 end
--- app/controllers/titolari_controller.rb
+++ app/controllers/titolari_controller.rb
@@ -69,6 +69,6 @@ class TitolariController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def titolare_params
-      params.require(:titolare).permit(:piva)
+      params.require(:titolare).permit(:nome,:cognome,:cf,:data_nascita,:telefono,:email,:piva,:actable_id,:actable_type)
     end
 end
