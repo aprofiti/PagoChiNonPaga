2f6cca9042dce861e37286bed23a88bf393c0e75
Alessandro Profiti <aprofiti@users.noreply.github.com>
2016-11-28 17:11:35 +0100

    Aggiunto metodo getNome al modello di Utente

--- app/models/utente.rb
+++ app/models/utente.rb
@@ -1,8 +1,25 @@
 class Utente < ActiveRecord::Base
-
+  # Entita' di partenza per l'IS-A
   actable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Ritorna il nome dell'Utente
+  # Viene utilizzato nelle view
+  def getNome
+    id = self.actable_id
+    tipo = self.actable_type
+
+    # Ricerco l'utente nella classe di apparteneza (IS-A nel database)
+    if tipo == "Cliente"
+      utente = Cliente.find(id)
+    elsif tipo == "Titolare"
+      utente = Titolare.find(id)
+    end
+    # Ritorna il nome
+    utente.nome
+  end
+
 end
