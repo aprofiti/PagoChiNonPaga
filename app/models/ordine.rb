d5a8000cee7e6eca2f7a7f4d3702cb36e4fb0925
Alessandro Profiti <aprofiti@users.noreply.github.com>
2016-11-30 20:37:30 +0100

    #32 Aggiunti Metodi Ausiliari a Ordine

--- app/models/ordine.rb
+++ app/models/ordine.rb
@@ -3,6 +3,11 @@ class Ordine < ActiveRecord::Base
   belongs_to :impresa
   has_and_belongs_to_many :prodotti
 
+  def getOrdini
+  #  @ordini.where("cliente_id = " + utente.actable_id.to_s + "")
+  #  @ordini.where("cliente_id = 1")
+  end
+
   # Necessario per mostrare il nome dell'Entita in RailsAdmin
   def name
     "#" + self.id.to_s + "Cliente: " + self.cliente_id.to_s
