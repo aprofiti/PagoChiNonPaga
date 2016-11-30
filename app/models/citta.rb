8cc18683628434cec7eca1296c25cd76eccf51aa
Alessandro Profiti <aprofiti@users.noreply.github.com>
2016-11-30 20:36:03 +0100

    #32 Aggiunti Metodi Ausiliari a Citta

--- app/models/citta.rb
+++ app/models/citta.rb
@@ -3,8 +3,18 @@ class Citta < ActiveRecord::Base
   has_many :imprese
   has_many :indirizzi
 
-  # Necessario per mostrare il nome dell'Entita in RailsAdmin
-  def name
+  # Ritorna la lista delle imprese registrate presso la citta
+  def getImprese
+    self.imprese
+  end
+
+  def getNome
     self.nome
   end
+
+  # Necessario per mostrare il nome dell'Entita in RailsAdmin
+  def name
+    getNome
+  end
+
 end
