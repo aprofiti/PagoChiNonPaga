091c3473a8497d039510d6b17430a06d5dba9380
Alessandro Profiti <aprofiti@users.noreply.github.com>
2016-11-30 20:37:10 +0100

    #32 Aggiunti Metodi Ausiliari a Polo

--- app/models/polo.rb
+++ app/models/polo.rb
@@ -2,9 +2,23 @@ class Polo < ActiveRecord::Base
   has_many :citta
   has_many :admins
 
+  def getCitta
+    self.citta
+  end
+
+  def getImprese
+    # Ritorna una collezione, trovare un modo per risolvere
+
+    #listaCitta = self.getCitta
+    #getImprese
+  end
+
+  def getAdmins
+    self.admins
+  end
   # Necessario per mostrare il nome dell'Entita in RailsAdmin
   def name
     self.nome
   end
-  
+
 end
