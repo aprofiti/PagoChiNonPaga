cf388d49f7ae25fb53fcca3c8a1abe14a6f3818b
Alessandro Profiti <aprofiti@users.noreply.github.com>
2016-12-14 16:55:55 +0100

    #12 Aggiunti modello del carrello e l'associazione per i prodotto

--- app/models/carrello_prodotto.rb
+++ app/models/carrello_prodotto.rb
@@ -2,7 +2,13 @@ class CarrelloProdotto < ActiveRecord::Base
   # Gemma Carrello
   acts_as_shopping_cart_item_for :carrello
 
+  # Necessario per mostrare il nome dell'Entita in RailsAdmins
   def name
+    id = self.item_id
+    # Controllo che ci sia il record prima di visualizzare nel pannello
+    if id != nil
+      Prodotto.find(id).name
+    end
   end
 
 end
