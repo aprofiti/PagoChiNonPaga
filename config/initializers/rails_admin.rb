d63ff87b75efaaf45cc06939250879f3376e2270
Alessandro Profiti <aprofiti@users.noreply.github.com>
2017-01-16 19:08:06 +0100

    #23 Riorganizzata Barra di Navigazione di Rails Admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,7 +1,6 @@
 RailsAdmin.config do |config|
 
   ### Popular gems integration
-
   ## == Devise ==
    config.authenticate_with do
      warden.authenticate! scope: :admin
@@ -16,18 +15,104 @@ RailsAdmin.config do |config|
 
   ## == PaperTrail ==
    config.audit_with :paper_trail, 'Admin', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
 
+  #
+  # Configurazione delle sezioni della Barra di Navigazione
+  #
+
+  # Sezione Amministratori
+  config.model 'Polo' do
+    navigation_label 'Sezione Amministratori'
+    navigation_icon 'fa fa-map-marker'
+    weight 0
+  end
+
+  config.model 'Citta' do
+    parent Polo
+    navigation_icon 'fa fa-globe'
+  end
+
+  # Sezione Utenti
+  config.model 'Utente' do
+    navigation_label 'Utenti del Sistema'
+    navigation_icon 'fa fa-users'
+    weight +3
+  end
+
+  config.model 'Admin' do
+    parent Utente
+    navigation_icon 'fa fa-user-secret'
+  end
+
+  config.model 'Titolare' do
+    parent Utente
+    navigation_icon 'fa fa-user'
+  end
+
+  config.model 'Cliente' do
+    parent Utente
+    navigation_icon 'fa fa-user'
+  end
+
+  # Sezione Imprese
+  config.model 'Impresa' do
+    navigation_label 'Sezione Imprese'
+    navigation_icon 'fa fa-industry'
+    weight +1
+  end
+
+  config.model 'Prodotto' do
+    parent Impresa
+    navigation_icon 'fa fa-shopping-bag'
+  end
+
+  config.model 'Categoria' do
+    parent Impresa
+    navigation_icon 'fa fa-list'
+    weight +1
+  end
+
+  config.model 'Sottocategoria' do
+    parent Impresa
+    navigation_icon 'fa fa-list'
+    weight +2
+  end
+
+  # Sezione Ecommerce
+  config.model 'Carrello' do
+    navigation_label 'Sezione Ecommerce'
+    navigation_icon 'fa fa-shopping-cart'
+    weight +2
+  end
+
+  config.model 'Ordine' do
+    parent Carrello
+    navigation_icon 'fa fa-euro'
+  end
+
+  config.model 'CarrelloProdotto' do
+    parent Carrello
+    navigation_icon 'fa fa-cart-arrow-down'
+  end
+
+  config.model 'StatoOrdine' do
+    parent Carrello
+    navigation_icon 'fa fa-truck'
+  end
+
+
+  # Configuro le azioni che si possono utilizzare nel pannello
   config.actions do
     dashboard                     # mandatory
     index                         # mandatory
 
-    # Rimuove la possibilita di creare un nuovo Cliente o Titoare via pannello
+    # Rimuove la possibilita' di creare un nuovo Cliente o Titolare via pannello
+    # Incompatibilita' con la gemma 'active_record-acts_as'
     new do
       except [Cliente, Titolare]
     end
-    
+
     export
     bulk_delete
     show
@@ -39,4 +124,5 @@ RailsAdmin.config do |config|
      history_index
      history_show
   end
+
 end
