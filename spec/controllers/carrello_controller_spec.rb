19a45a233b08813550701749d3a875652aec61dd
Lorenzo Ricelli <lorenzo.ricelli@gmail.com>
2017-01-17 20:31:27 +0100

    Test Carrello e pulizia file test

--- spec/controllers/carrello_controller_spec.rb
+++ spec/controllers/carrello_controller_spec.rb
@@ -20,140 +20,31 @@ require 'rails_helper'
 
 RSpec.describe CarrelloController, type: :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # Carrello. As you add validations to Carrello, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  it "should not get carrello" do
+    cliente= createCliente("mario","rossi")
+    carrello = Carrello.create(cliente_id: cliente.id)
+    get :show, id: cliente.carrello
+    expect(response).to_not render_template :show
+   end
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # CarrelloController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
-
-  describe "GET #index" do
-    it "assigns all carrello as @carrello" do
-      carrello = Carrello.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(assigns(:carrello)).to eq([carrello])
-    end
+  it "should get carrello" do
+    cliente= createCliente("mario","rossi")
+    carrello = Carrello.create(cliente_id: cliente.id)
+    sign_in cliente
+    get :show,  id: cliente.carrello
+    expect(response).to render_template :show
   end
 
-  describe "GET #show" do
-    it "assigns the requested carrello as @carrello" do
-      carrello = Carrello.create! valid_attributes
-      get :show, params: {id: carrello.to_param}, session: valid_session
-      expect(assigns(:carrello)).to eq(carrello)
-    end
-  end
-
-  describe "GET #new" do
-    it "assigns a new carrello as @carrello" do
-      get :new, params: {}, session: valid_session
-      expect(assigns(:carrello)).to be_a_new(Carrello)
-    end
-  end
-
-  describe "GET #edit" do
-    it "assigns the requested carrello as @carrello" do
-      carrello = Carrello.create! valid_attributes
-      get :edit, params: {id: carrello.to_param}, session: valid_session
-      expect(assigns(:carrello)).to eq(carrello)
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Carrello" do
-        expect {
-          post :create, params: {carrello: valid_attributes}, session: valid_session
-        }.to change(Carrello, :count).by(1)
-      end
-
-      it "assigns a newly created carrello as @carrello" do
-        post :create, params: {carrello: valid_attributes}, session: valid_session
-        expect(assigns(:carrello)).to be_a(Carrello)
-        expect(assigns(:carrello)).to be_persisted
-      end
-
-      it "redirects to the created carrello" do
-        post :create, params: {carrello: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(Carrello.last)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns a newly created but unsaved carrello as @carrello" do
-        post :create, params: {carrello: invalid_attributes}, session: valid_session
-        expect(assigns(:carrello)).to be_a_new(Carrello)
-      end
-
-      it "re-renders the 'new' template" do
-        post :create, params: {carrello: invalid_attributes}, session: valid_session
-        expect(response).to render_template("new")
-      end
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested carrello" do
-        carrello = Carrello.create! valid_attributes
-        put :update, params: {id: carrello.to_param, carrello: new_attributes}, session: valid_session
-        carrello.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "assigns the requested carrello as @carrello" do
-        carrello = Carrello.create! valid_attributes
-        put :update, params: {id: carrello.to_param, carrello: valid_attributes}, session: valid_session
-        expect(assigns(:carrello)).to eq(carrello)
-      end
-
-      it "redirects to the carrello" do
-        carrello = Carrello.create! valid_attributes
-        put :update, params: {id: carrello.to_param, carrello: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(carrello)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns the carrello as @carrello" do
-        carrello = Carrello.create! valid_attributes
-        put :update, params: {id: carrello.to_param, carrello: invalid_attributes}, session: valid_session
-        expect(assigns(:carrello)).to eq(carrello)
-      end
-
-      it "re-renders the 'edit' template" do
-        carrello = Carrello.create! valid_attributes
-        put :update, params: {id: carrello.to_param, carrello: invalid_attributes}, session: valid_session
-        expect(response).to render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested carrello" do
-      carrello = Carrello.create! valid_attributes
-      expect {
-        delete :destroy, params: {id: carrello.to_param}, session: valid_session
-      }.to change(Carrello, :count).by(-1)
-    end
-
-    it "redirects to the carrello list" do
-      carrello = Carrello.create! valid_attributes
-      delete :destroy, params: {id: carrello.to_param}, session: valid_session
-      expect(response).to redirect_to(carrello_index_url)
-    end
-  end
+  it "should not get other cliente carrello" do
+    cliente1= createCliente("mario","rossi")
+    carrello1 = Carrello.create(cliente_id: cliente1.id)
+    cliente2= createCliente("mario","bianchi")
+    cliente1= createCliente("mario","rossi")
+    carrello1 = Carrello.create(cliente_id: cliente1.id)
+    carrello2 = Carrello.create(cliente_id: cliente2.id)
+    sign_in cliente1
+    get :show, id: cliente2.carrello
+    expect(response).to_not render_template :show
+   end
 
 end
