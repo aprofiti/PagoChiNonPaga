219388856a5c97a4055211c74b2b37f5caec73cf
Alessandro Profiti <aprofiti@users.noreply.github.com>
2017-01-23 16:21:26 +0100

    Aggiunto riquadro per i messaggi via rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  # Helper usato nelle views per messaggi Flash
+  def bootstrap_class_for (flash_type)
+    case flash_type
+    when "success"
+        "alert-success" # Green
+      when "error"
+        "alert-danger" # Red
+      when "alert"
+        "alert-warning" # Yellow
+      when "notice"
+        "alert-info" # Blue
+      else
+        flash_type.to_s
+    end
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      tipo = bootstrap_class_for(msg_type)
+      concat(content_tag(:div, message, class: "alert #{tipo} fade in") do
+              concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+              concat message
+            end)
+    end
+    nil
+  end
 end
