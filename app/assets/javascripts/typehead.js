8de30867515a834c4371b9a50988e182f1ba117a
Lorenzo Ricelli <lorenzo.ricelli@gmail.com>
2017-01-24 23:04:54 +0100

    Fix a ricerca Typeahead

--- app/assets/javascripts/typehead.js
+++ app/assets/javascripts/typehead.js
@@ -1,5 +1,6 @@
 // Inizializza typehead
 $(function() {
+  var selected= false;                              //indica se c'è stata una selezione e un autocomplete con il tab
   var initialize_imprese_typeahead;
   initialize_imprese_typeahead = function() {
     var imprese_typeahead;
@@ -20,16 +21,25 @@ $(function() {
       source: imprese_typeahead.ttAdapter(),
       // Modifica l'aspetto del finestra dei risultati
       templates: {
+        //empty indica il messaggio che viene mostrato se scrivo qualcosa che non è in source
         empty: [
-                '<div class="empty-message">',
-                'Nessuna impresa corrispondente',
-                '</div>'
+                '<div class="empty-message">','Nessuna impresa corrispondente','</div>'
             ].join('\n'),
         suggestion: Handlebars.compile("<div> {{#if nome}} <strong>Impresa:</strong> {{nome}} {{/if}} {{#if citta}} <strong>Citta:</strong> {{citta}} {{/if}}  </div>")
       }
-    }).on('typeahead:selected ', function (e, datum) {
+    }).on('typeahead:selected ', function (e, datum) {        //evento elemento selezionato dal dropdown
+        selected= true;
         document.getElementById('id_imp').value = datum.id;
-    });
+    }).on('typeahead:autocompleted ', function (e, datum) {  //evento elemento autocompletato da suggerimento
+        selected= true;
+        document.getElementById('id_imp').value = datum.id;
+      }).on('keypress', function(e) {                        //lanciato se premuto un tasto, keycode 13 sta per Enter
+            if(e.keyCode == 13) {
+                if(selected) return true;
+                else  e.preventDefault();
+                $('#typeahead').typeahead('close');
+            }
+        });
   };
   return initialize_imprese_typeahead();
 });
