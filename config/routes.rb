c9449c77089834e6ba5a9a39d6b9a97e175e2cec
Lorenzo Ricelli <lorenzo.ricelli@gmail.com>
2017-01-25 18:54:25 +0100

    Permessi punti nella rotta

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   # Rotte per RailsAdmin
   mount RailsAdmin::Engine => '/cp', as: 'rails_admin'
 
-  resources :poli, except: [:new,:edit], param: :nome
+  resources :poli, except: [:new,:edit], param: :nome, :nome => /[^\/]+/
   devise_for :utenti, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout"}
   devise_for :admins
-  resources :citta, except: [:new,:edit] , param: :nome
+  resources :citta, except: [:new,:edit] , param: :nome , :nome => /[^\/]+/
   resources :ordini, except: :new, path: '/mieiOrdini' do
     member do
       post :prepara_ordini
@@ -19,18 +19,18 @@ Rails.application.routes.draw do
   get "imprese/autocomplete" => "imprese#autocomplete"
 
   #PATH del tipo /imprese/:nome/prodotti/:nome
-  resources :imprese, param: :nome do
-    resources :prodotti, param: :nome
+  resources :imprese, param: :nome , :nome => /[^\/]+/ do
+    resources :prodotti, param: :nome, :nome => /[^\/]+/
   end
 
   #PATH del tipo /categorie/:nome/sottocategorie/:nome
-  resources :categorie, except: [:show,:new,:edit], param: :nome do  #Il parametro ora è il nome e non l'id
-    resources :sottocategorie, except: [:new,:edit], param: :nome    #Il parametro ora è il nome e non l'id
+  resources :categorie, except: [:show,:new,:edit], param: :nome, :nome => /[^\/]+/ do  #Il parametro ora è il nome e non l'id
+    resources :sottocategorie, except: [:new,:edit], param: :nome , :nome => /[^\/]+/   #Il parametro ora è il nome e non l'id
   end
 
   resources :titolari, except: [:index]
   resources :clienti, except: [:index]
-  
+
 
   resources :carrello, except: [:index, :new] do
     member do
