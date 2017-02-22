ff8727138180fcc90943eee0b7d956758ecaccbf
Alessandro Profiti <aprofiti@users.noreply.github.com>
2017-02-22 11:41:09 +0100

    FIX reindirizzamento dopo login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,27 +5,34 @@ class ApplicationController < ActionController::Base
   before_filter :mailer_set_url_options
 
 
-#Quando posso faccio redirect a pagina precedente
-#Quando invece ho errore (ActionController::RedirectBackError) "No HTTP_REFERER was set ..."
-#faccio redirect a root
-
+  # Quando posso faccio redirect a pagina precedente
+  # Quando invece ho errore (ActionController::RedirectBackError) "No HTTP_REFERER was set ..."
+  # faccio redirect a root
   def redirect_back
     redirect_to :back
   rescue ActionController::RedirectBackError
     redirect_to root_path
   end
 
+  # Reindirizza dopo il login al Pannello di Controllo corrispondente alla tipologia dell'Utente
   def after_sign_in_path_for(resource)
-    if current_admin
-      rails_admin_path
-    elsif (current_utente.isCliente? )
-      cliente_path(current_utente.actable_id)
-    elsif (current_utente.isTitolare? )
-      if current_utente.sign_in_count == 1
-        new_impresa_path
-      else
-      titolare_path(current_utente.actable_id)
+    if current_utente
+      # Reindirizzo al Profilo Utente
+      if (current_utente.isCliente?)
+        cliente_path(current_utente.actable_id)
+      elsif (current_utente.isTitolare?)
+        idTitolare = current_utente.actable_id
+        titolare = Titolare.find(idTitolare)
+        # Se il titolare non ha imprese registrate, viene reindirizzato alla creazione di una Impresa
+        if titolare.getNumImprese == 0
+          new_impresa_path
+        else
+          titolare_path(idTitolare)
+        end
       end
+    elsif current_admin
+        # Reindirizzo al Pannello di Controllo degli Admin
+        rails_admin_path
     end
   end
 
