2f6655856159a496616cb4663056379658e27e74
Alessandro Profiti <aprofiti@users.noreply.github.com>
2017-03-05 15:53:28 +0100

    Fix Path immagini di default per Carrierwave

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -57,10 +57,10 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    if  model.class.name == "Prodotto"
-      "/assets/template/missing.png"
+    if model.class.name == "Prodotto"
+      ActionController::Base.helpers.asset_path("template/" + [version_name, "missing.png"].compact.join('_'))
     else
-      "/assets/template/Sfondo Negozio.png"
+      ActionController::Base.helpers.asset_path("template/" + [version_name, "Sfondo Negozio.png"].compact.join('_'))
     end
   end
 
