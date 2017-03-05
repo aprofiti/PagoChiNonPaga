26e394171976a235636caf712cc5a5d712a9acc8
Alessandro Profiti <aprofiti@users.noreply.github.com>
2017-03-05 15:08:39 +0100

    test path img

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -58,9 +58,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
     if  model.class.name == "Prodotto"
-      "/assets/images/template/missing.png"
+      image_path("template/missing.png")     
     else
-      "/assets/images/template/Sfondo Negozio.png"
+      image_path("template/Sfondo Negoziopng") 
     end
   end
 
